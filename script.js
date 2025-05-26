7003a7f5fb8c49a81055a8bf681fb20eeecbaf69
abhi-shock <shockabhi35@gmail.com>
2025-05-26 18:25:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,18 +4,27 @@ const btn = document.querySelector(".btn");
 const result = document.querySelector(".result");
 
 btn.addEventListener("click", function () {
+  const substraction = function (a, b) {
+    if (a > b) {
+      return a - b;
+    } else if (a < b) {
+      return b - a;
+    }
+  };
   if (input.value == "") {
     alert("enter your date of birth");
   } else {
     //birth year date
+
     const today = new Date();
     const birthdate = new Date(input.value);
 
     const year = today.getFullYear() - birthdate.getFullYear();
-    const month = today.getMonth() - birthdate.getMonth();
-    const day = today.getDate() - birthdate.getDate();
+    const month = substraction(today.getMonth(), birthdate.getMonth());
+    const day = substraction(today.getDate(), birthdate.getDate());
     console.log(year);
 
     result.innerHTML = `your age is ${year} years !! ${month} months !! ${day} days`;
   }
 });
+
